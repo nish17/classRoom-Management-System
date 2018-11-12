fcce2660ffeb57ab8c8b126d2dfcb8b468389b9a
nish17 <nimesh3536@gmail.com>
2018-11-12 22:28:48 +0530

    works but sql query returns json

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
-
+const path = require("path");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Password@123",
+  password: "Password@123"
   database: "newClassRoom"
 });
 
@@ -14,17 +16,27 @@ connection.connect(err => {
     console.log("Something went wrong, " + err);
   } else console.log("Connected to the database");
 });
+// function SubmitQuery() {
 app.get("/", (req, res) => {
-  connection.query("SELECT * FROM classStructure", (err, rows, fields) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/sql-query", (req, res) => {
+  const { sql } = req.body;
+  connection.query(sql, (err, rows, fields) => {
     if (err) {
       console.log(`Error in the query, ${err}`);
+      res.send("Error in the query");
     } else {
       console.log("Success!");
-      res.json(fields);
-      res.json(rows);
+      // res.json(fields);
+      res.send(rows);
     }
   });
+  // res.send(req.body.sql);
 });
+
+// }
 app.listen("8080", () => {
   console.log("Server is up and running on port 8080");
 });
